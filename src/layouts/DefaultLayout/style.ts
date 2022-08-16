4aef3a6f64413b5447ffa1b20478f4d03572c86d
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-08-16 20:48:31 -0300

    refactor: DefaultLayout

---
+++ src/layouts/DefaultLayout/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  background: ${(props) => props.theme.background};
+  display: flex;
+  flex-direction: column;
+  margin-top: 120px; //temporário
+`

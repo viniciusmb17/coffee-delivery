2f37fa813729eef808f279b2c16a6969ec03b965
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-08-16 20:49:33 -0300

    style: Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,30 @@
-import { HeaderContainer } from './style'
+import {
+  ActionsContainer,
+  CartContainer,
+  HeaderContainer,
+  LocationContainer,
+} from './style'
 
 import logoCoffeeDelivery from '../../assets/logo-coffee-delivery.svg'
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logoCoffeeDelivery} alt="" />
       <nav>
-        <span>Localização</span>
-        <span>Carrinho de compras</span>
+        <NavLink to="/">
+          <img src={logoCoffeeDelivery} alt="" />
+        </NavLink>
+        <ActionsContainer>
+          <LocationContainer>
+            <MapPin size={22} weight="fill" />
+            <span>Porto Alegre, RS</span>
+          </LocationContainer>
+          <CartContainer to="/checkout">
+            <ShoppingCart size={22} weight="fill" />
+          </CartContainer>
+        </ActionsContainer>
       </nav>
     </HeaderContainer>
   )
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,10 +1,79 @@
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background: ${(props) => props.theme.background};
   display: flex;
+  flex-direction: row;
   align-items: center;
   justify-content: space-between;
+  padding: 2rem 10rem;
+  line-height: 130%;
 
   font-family: 'Baloo 2', cursive;
   font-weight: 700;
+
+  img {
+    max-width: 5.309rem;
+    max-height: 2.5rem;
+    width: 100%;
+    height: 100%;
+  }
+  nav {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 90rem;
+    width: 100%;
+  }
+`
+export const ActionsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  gap: 12px;
+`
+
+export const LocationContainer = styled.div`
+  background: ${(props) => props.theme['purple-light']};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  font-family: 'Roboto', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.875rem;
+
+  padding: 0.5rem;
+  gap: 0.25rem;
+  border-radius: 6px;
+  color: ${(props) => props.theme.purple};
+
+  span {
+    color: ${(props) => props.theme['purple-dark']};
+    white-space: nowrap;
+  }
+`
+export const CartContainer = styled(NavLink)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  min-width: 2.375rem;
+  width: 100%;
+  min-height: 2.375rem;
+  height: 100%;
+  border-radius: 6px;
+
+  background: ${(props) => props.theme['yellow-light']};
+  color: ${(props) => props.theme['yellow-dark']};
 `

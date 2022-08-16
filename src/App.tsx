4e945b16f69b7512481eb8e50b0493655a400feb
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-08-16 19:55:15 -0300

    fix: add ThemeProvider on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { Router } from './Router'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 

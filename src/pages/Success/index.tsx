421e24f245e856a2aba8991d46e1a9b9d465b039
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-09-20 10:45:51 -0300

    style: Success component

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,41 +1,71 @@
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 import SuccessDeliveryIllustration from '../../assets/success-delivery-illustration.png'
+import {
+  SuccessContainer,
+  SuccessMainSection,
+  SuccessImageSection,
+  MainTitles,
+  OrderInfo,
+  LocalInfo,
+  TimeInfo,
+  PaymentInfo,
+  LocalIcon,
+  TimeIcon,
+  PaymentIcon,
+} from './style'
 
 export function Success() {
   return (
-    <>
-      <section>
-        <div>
-          <h1>Uhu! Pedido confirmado</h1>
-          <h2>Agora é só aguardar que logo o café chegará até você</h2>
-        </div>
-        <div>
-          <div>
+    <SuccessContainer>
+      <MainTitles>
+        <h1>Uhu! Pedido confirmado</h1>
+        <h2>Agora é só aguardar que logo o café chegará até você</h2>
+      </MainTitles>
+      <SuccessMainSection>
+        <OrderInfo>
+          <LocalInfo>
+            <LocalIcon>
+              <MapPin size={16} weight="fill" />
+            </LocalIcon>
             <div>
               <span>
                 Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
               </span>
+              <br />
               <span>Farrapos - Porto Alegre, RS</span>
             </div>
+          </LocalInfo>
+          <TimeInfo>
+            <TimeIcon>
+              <Timer size={16} weight="fill" />
+            </TimeIcon>
             <div>
-              <span>Previsão de entrega</span>
               <span>
+                Previsão de entrega
+                <br />
                 <strong>20 min - 30 min</strong>
               </span>
             </div>
+          </TimeInfo>
+          <PaymentInfo>
+            <PaymentIcon>
+              <CurrencyDollar size={16} weight="fill" />
+            </PaymentIcon>
             <div>
               <span>Pagamento na entrega</span>
+              <br />
               <span>
                 <strong>Cartão de crédito</strong>
               </span>
             </div>
+          </PaymentInfo>
+        </OrderInfo>
+        <SuccessImageSection>
+          <div>
+            <img src={SuccessDeliveryIllustration} alt="" />
           </div>
-        </div>
-      </section>
-      <section>
-        <div>
-          <img src={SuccessDeliveryIllustration} alt="" />
-        </div>
-      </section>
-    </>
+        </SuccessImageSection>
+      </SuccessMainSection>
+    </SuccessContainer>
   )
 }
---
+++ src/pages/Success/style.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components'
+
+export const SuccessContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  left: calc(50% - 16.0625rem / 2 - 28.9688rem);
+  position: relative;
+`
+export const SuccessMainSection = styled.section`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  column-gap: 6.375rem;
+`
+export const MainTitles = styled.div`
+  h1 {
+    font-family: 'Baloo 2', cursive;
+    font-weight: 800;
+    font-size: 2rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['yellow-dark']};
+  }
+  h2 {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 130%;
+    color: ${(props) => props.theme['base-subtitle']};
+    font-stretch: 100;
+  }
+
+  // Cálculo para totalizar diferença de 40px entre títulos e infos considerando
+  // align-items de OrderInfo
+  margin-bottom: calc(2.5rem - 0.7188rem);
+`
+export const OrderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 2.5rem;
+  gap: 2rem;
+  width: 32.875rem;
+  height: 16.875rem;
+
+  border: 1px solid red;
+  border-radius: 6px 36px;
+`
+export const ItemInfo = styled.div`
+  display: flex;
+  column-gap: 0.75rem;
+  align-items: center;
+
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 130%;
+`
+export const LocalInfo = styled(ItemInfo)``
+export const TimeInfo = styled(ItemInfo)``
+export const PaymentInfo = styled(ItemInfo)``
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${(props) => props.theme.background};
+
+  width: 2rem;
+  height: 2rem;
+
+  border-radius: 33px;
+
+  flex-grow: 0;
+  flex: none;
+`
+export const LocalIcon = styled(IconContainer)`
+  background-color: ${(props) => props.theme.purple};
+`
+export const TimeIcon = styled(IconContainer)`
+  background-color: ${(props) => props.theme.yellow};
+`
+export const PaymentIcon = styled(IconContainer)`
+  background-color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const SuccessImageSection = styled.section`
+  display: flex;
+  position: relative;
+
+  width: 30.75rem;
+  height: 18.3125rem;
+`

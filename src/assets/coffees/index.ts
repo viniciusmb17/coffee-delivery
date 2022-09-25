aaa38a2c252ab0f048c47bda2b20427c2f9d55e2
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-09-25 19:10:09 -0300

    feat: working on CoffeeList component

---
+++ src/assets/coffees/index.ts
@@ -0,0 +1,31 @@
+import AmericanoImg from './americano.png'
+import ArabeImg from './arabe.png'
+import CafeComLeiteImg from './cafe-com-leite.png'
+import CafeGeladoImg from './cafe-gelado.png'
+import CappuccinoImg from './capuccino.png'
+import ChocolateQuenteImg from './chocolate-quente.png'
+import CubanoImg from './cubano.png'
+import ExpressoCremosoImg from './expresso-cremoso.png'
+import ExpressoImg from './expresso.png'
+import HavaianoImg from './havaiano.png'
+import IrlandesImg from './irlandês.png'
+import LatteImg from './latte.png'
+import MacchiatoImg from './macchiato.png'
+import MochaccinoImg from './mochaccino.png'
+
+export {
+  AmericanoImg,
+  ArabeImg,
+  CafeComLeiteImg,
+  CafeGeladoImg,
+  CappuccinoImg,
+  ChocolateQuenteImg,
+  CubanoImg,
+  ExpressoCremosoImg,
+  ExpressoImg,
+  HavaianoImg,
+  IrlandesImg,
+  LatteImg,
+  MacchiatoImg,
+  MochaccinoImg,
+}
